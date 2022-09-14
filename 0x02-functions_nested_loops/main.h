9a475bdfb1584aa14895056280eb0aa7898d9094
nigusgetachew <nigus567@gmail.com>
2022-09-14 06:32:21 -0700

    -islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,47 @@
-#ifndef HOLEERTON_H_INCLUDED
-#difine HOLEERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
- int _putcher(char);
+int _putchar(char);
+
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
 int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
+
 int _abs(int);
+
 int print_last_digit(int);
-void jack_baver(void)
-void time_table (void);
-int add(int a,int b);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
 void print_to_98(int n);
+
 void print_times_table(int n);
-void num(int r,int int n)
+
+void num(int r, int n);
 
 #endif
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+cler
+
+
+
+
+
+
+
