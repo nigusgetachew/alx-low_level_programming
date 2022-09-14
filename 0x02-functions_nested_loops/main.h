4a0548e8e330c668c08944bb6903c909425e182c
nigusgetachew <nigus567@gmail.com>
2022-09-14 05:10:34 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLEERTON_H_INCLUDED
+#difine HOLEERTON_H_INCLUDED
+
+ int _putcher(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_baver(void)
+void time_table (void);
+int add(int a,int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r,int int n)
+
+#endif
