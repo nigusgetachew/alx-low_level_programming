f15f594178d17a435ebe8685e776099013772379
nigusgetachew <111353986+nigusgetachew@users.noreply.github.com>
2022-09-15 13:43:15 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,56 +1,13 @@
-<<<<<<< HEAD
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-
-int _putchar(char);
-
-=======
-
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-
-int _putchar(char);
-
->>>>>>> 4f0fdb27c14740a6f291a0e5f6dbd3b49dcdee52
+int _putchar(char c);
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
 int _abs(int);
-
 int print_last_digit(int);
-
 void jack_bauer(void);
-
 void times_table(void);
-
-int add(int a, int b);
-
+int add(int, int);
 void print_to_98(int n);
-
 void print_times_table(int n);
-
-void num(int r, int n);
-
-#endif
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-cler
-
-
-
-
-
-
-
