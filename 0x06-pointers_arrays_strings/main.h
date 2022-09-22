cd8a87ee8ffc96d445deb159bdcd95f1c737af4a
nigusgetachew <111353986+nigusgetachew@users.noreply.github.com>
2022-09-22 09:10:10 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,33 +1,18 @@
 #ifndef MAIN_H_INCUDED
-
 #define MAIN_H_INCLUDED
 
-
-
 int _putchar(char);
-
 char *_strcat(char *, char *);
-
 char *_strncat(char *,char *,int);
-
 char *_strncpy(char *, char *, int);
-
 int _strcmp(char *, char *);
-
 void reverse_array(int *,int);
-
 char *string_toupper(char *);
-
 char *cap_string(int *str);
-
 char *leet(char *);
-
 char *rot13(char *);
-
 void print_number(int);
-
 char *infinite_add(char *, char *,char *, int);
-
-
+void print_buffer(char *, int);
 
 #endif
